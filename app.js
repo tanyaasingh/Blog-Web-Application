9915691594d850b71daf247c06f55a847f3503fc
Tanyaa Singh <tanyaasinghts@gmail.com>
2018-10-22 02:15:42 +0530

    blog app

---
+++ app.js
@@ -0,0 +1,99 @@
+var bodyParser = require("body-parser"),
+methodOverride = require("method-override"),
+expressSanitizer = require("express-sanitizer"),
+mongoose       = require("mongoose"),
+express        = require("express"),
+app            = express();
+
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+app.get("/", function(req, res){
+   res.redirect("/blogs"); 
+});
+
+app.get("/blogs", function(req, res){
+   Blog.find({}, function(err, blogs){
+       if(err){
+           console.log("ERROR!");
+       } else {
+          res.render("index", {blogs: blogs}); 
+       }
+   });
+});
+
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+app.post("/blogs", function(req, res){
+    
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        } else {
+            //then, redirect to the index
+            res.redirect("/blogs");
+        }
+    });
+});
+
+app.get("/blogs/:id", function(req, res){
+   Blog.findById(req.params.id, function(err, foundBlog){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.render("show", {blog: foundBlog});
+       }
+   })
+});
+
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+})
+
+
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+      if(err){
+          res.redirect("/blogs");
+      }  else {
+          res.redirect("/blogs/" + req.params.id);
+      }
+   });
+});
+
+app.delete("/blogs/:id", function(req, res){
+   //destroy blog
+   Blog.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs");
+       }
+   })
+   //redirect somewhere
+});
+app.listen(4444, function () {
+    console.log("Server started on http://localhost:4444");
+});
